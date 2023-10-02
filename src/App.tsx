2a16421cbbbbcb094194301338e6a1d8251ea446
unknown <pvsantos01@gmail.com>
2023-10-02 14:12:53 -0300

    iniciando

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
 import "./App.css";
 
 function App() {
-  const name = "Paulo Vitor 02";
-  const pessoas = [
-    { nome: "Paulo Vitor", idade: 35 },
-    { nome: "Ronaldinho", idade: 44 },
-    { nome: "Edmundo", idade: 55 },
-    { nome: "Romário", idade: 38 },
-  ];
-  return (
-    <>
-      <p className="primeironome">{name}</p>
-      {pessoas.map((element) => (
-        <div>
-          <p>{element.nome}</p>
-          <p>{element.idade}</p>
-        </div>
-      ))}
-    </>
-  );
+  return <>Paulo Vitor</>;
 }
 
 export default App;
